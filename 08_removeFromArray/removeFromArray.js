070be1651328b719248a71a72050b2f9a379c4e3
valeryia kazyrytskaya <183213488+kkkoyari@users.noreply.github.com>
2025-10-24 19:39:11 +0300

    Complete the removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+	let competelyNewArray = [];
+	let originalArray = array;
+
+	for (let i = 0; i < args.length; i++) {
+		let itemValue = args[i];
+		competelyNewArray = originalArray.filter(item => item !== itemValue);
+		originalArray = competelyNewArray;
+	}
+	return competelyNewArray;
 };
 
 // Do not edit below this line
